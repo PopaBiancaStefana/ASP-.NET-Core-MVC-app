2da7b638a67dfe0697521c28d8a88d1cedf91d3a
POPA Bianca-Stefana <popabianca945@yahoo.com>
2025-03-26 12:58:09 +0200

    Add MVC base structure and first view

---
+++ PieShop/Models/Repositories/Interfaces/IPieRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace PieShop.Models.Repositories.Interfaces
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie? GetPieById(int id);
+    }
+}
--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -1,6 +1,12 @@
+using PieShop.Models.Repositories.Interfaces;
+using PieShop.Models.Repositories.Mocks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 
 var app = builder.Build();
 
